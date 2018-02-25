4aec278b5c221edc0370b90cee9c9791d5056af6
Charles Ballew <charleswballewjr@gmail.com>
2018-02-24 23:41:30 -0700

    peters movie-db

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db/knex.js')
+var knex = require('../db/knex.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("GET GOT!")
+  // console.log("GET GOT!")
+  // return knex.select().from('person')
+  return knex('person').where({'role': 'actor'})
+    .then(function(actors){
+      res.send(actors)
+    })
 })
 
 module.exports = router;
